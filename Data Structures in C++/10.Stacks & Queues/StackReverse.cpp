829afb8a79712278c1a19b97645607a22d916908
vive6271 <kumar6271vivek@gmail.com>
2023-02-04 00:17:13 +0530

    Update StackReverse.cpp

--- Data Structures in C++/10.Stacks & Queues/StackReverse.cpp
+++ Data Structures in C++/10.Stacks & Queues/StackReverse.cpp
@@ -41,28 +41,7 @@ Sample Output 2:
 #include <stack>
 using namespace std;
 
-void reverseStack(stack<int> &input, stack<int> &extra) {
-  if(input.size() <= 1){
-    return;
-  }
 
-  int lastElement = input.top();
-  input.pop();
-  reverseStack(input,extra);
-  while(!input.empty()){
-    int top = input.top();
-    input.pop();
-    extra.push(top);
-  }
-  input.push(lastElement);
-  while(!extra.empty()){
-    int top = extra.top();
-    extra.pop();
-    input.push(top);
-  }
-}
-
-/*
 void reverse(stack<int> &input, stack<int> &extra){
     if(input.empty()){
         return;
@@ -82,6 +61,7 @@ void reverseStack(stack<int> &input, stack<int> &extra) {
     }
 }
 
+/*
 //Not good.Because, We are swapping input to extra stack//
 
 void reverseStack(stack<int> &input, stack<int> &extra) {
